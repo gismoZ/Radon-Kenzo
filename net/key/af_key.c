684eaf0e853ddbd0dd2fb25f90cbf11fb80a54aa
Fabian Frederick <fabf@skynet.be>
2014-07-14 18:30:59 +0200

    af_key: remove unnecessary break after return
    Change-Id: I4e15550da0a0c0f2ca21477a9e4ac11b16cf877d
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/key/af_key.c
+++ net/key/af_key.c
@@ -398,7 +398,6 @@ static int verify_address_len(const void *p)
 		 * XXX When it can, remove this -EINVAL.  -DaveM
 		 */
 		return -EINVAL;
-		break;
 	}
 
 	return 0;
@@ -528,7 +527,6 @@ pfkey_satype2proto(uint8_t satype)
 		return IPPROTO_ESP;
 	case SADB_X_SATYPE_IPCOMP:
 		return IPPROTO_COMP;
-		break;
 	default:
 		return 0;
 	}
@@ -545,7 +543,6 @@ pfkey_proto2satype(uint16_t proto)
 		return SADB_SATYPE_ESP;
 	case IPPROTO_COMP:
 		return SADB_X_SATYPE_IPCOMP;
-		break;
 	default:
 		return 0;
 	}
