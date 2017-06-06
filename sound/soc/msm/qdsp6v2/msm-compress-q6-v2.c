e964f2bfba00930ea1a2e83f4d68e382d6612cd0
gismo <koumparilos@gmail.com>
2017-06-06 21:02:40 +0300

    Revert "ASoC: msm: add support for AVS 2.7 in native drivers"
    This reverts commit 95e8dcc72a2052cc525ef4fb8b9d4342a5268752.

--- sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
+++ sound/soc/msm/qdsp6v2/msm-compress-q6-v2.c
@@ -35,7 +35,6 @@
 
 #include <sound/timer.h>
 #include <sound/tlv.h>
-#include <sound/q6core.h>
 
 #include <sound/apr_audio-v2.h>
 #include <sound/q6asm-v2.h>
@@ -214,13 +213,8 @@ static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 				uint32_t volume_l, uint32_t volume_r)
 {
 	struct msm_compr_audio *prtd;
-	int i, rc = -1;
-	uint32_t avg_vol, gain_list[VOLUME_CONTROL_MAX_CHANNELS];
-	uint32_t num_channels;
-	struct snd_soc_pcm_runtime *rtd;
-	struct msm_compr_pdata *pdata;
-	bool use_default = true;
-	u8 *chmap = NULL;
+	int rc = 0;
+	uint32_t avg_vol;
 
 	pr_debug("%s: volume_l %d volume_r %d\n",
 		__func__, volume_l, volume_r);
@@ -228,30 +222,18 @@ static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 		pr_err("%s: session not active\n", __func__);
 		return -EPERM;
 	}
-	rtd = cstream->private_data;
 	prtd = cstream->runtime->private_data;
 
 	if (!prtd || !prtd->audio_client) {
-		pr_err("%s: invalid session/rtd, prtd or no audio client",
-				__func__);
+		pr_err("%s: invalid session prtd or no audio client", __func__);
 		return rc;
 	}
-	pdata = snd_soc_platform_get_drvdata(rtd->platform);
 
 	if (prtd->compr_passthr != LEGACY_PCM) {
 		pr_debug("%s: No volume config for passthrough %d\n",
 			 __func__, prtd->compr_passthr);
 		return rc;
 	}
-
-	use_default = !(pdata->ch_map[rtd->dai_link->be_id]->set_ch_map);
-	chmap = pdata->ch_map[rtd->dai_link->be_id]->channel_map;
-	num_channels = prtd->num_channels;
-	pr_debug("%s: call q6asm_set_volume volume_l(%d)volume_r(%d)num of channels(%d)\n",
-	__func__, volume_l, volume_r, prtd->num_channels);
-	pr_debug("%s: AVS version(%d): 0 for AVS2.6, 1 for AVS2.7\n",
-	__func__, q6core_get_avs_version());
-
 	if (prtd->num_channels > 2) {
 		/*
 		 * Currently the left and right gains are averaged an applied
@@ -262,51 +244,18 @@ static int msm_compr_set_volume(struct snd_compr_stream *cstream,
 		 * channel gains.
 		 *
 		 */
-		switch (q6core_get_avs_version()) {
-		case Q6_SUBSYS_AVS2_7:
-			avg_vol = (volume_l + volume_r) / 2;
-			for (i = 0; i < prtd->num_channels; i++)
-				gain_list[i] = avg_vol;
-			rc = q6asm_set_multich_gain(prtd->audio_client,
-				num_channels, gain_list, chmap, use_default);
-			break;
-		case Q6_SUBSYS_AVS2_6:
-			avg_vol = (volume_l + volume_r) / 2;
-			rc = q6asm_set_volume(prtd->audio_client, avg_vol);
-			break;
-		case Q6_SUBSYS_INVALID:
-		default:
-			pr_err("%s: UNKNOWN AVS IMAGE\n", __func__);
-			return rc;
-		}
+		pr_debug("%s: call q6asm_set_volume for multichannel\n",
+			 __func__);
+		avg_vol = (volume_l + volume_r) / 2;
+		rc = q6asm_set_volume(prtd->audio_client, avg_vol);
 	} else {
-		switch (q6core_get_avs_version()) {
-		case Q6_SUBSYS_AVS2_7:
-			gain_list[0] = volume_l;
-			gain_list[1] = volume_r;
-			/* force sending FR/FL/FC volume for mono */
-			if (prtd->num_channels == 1) {
-				gain_list[2] = volume_l;
-				num_channels = 3;
-				use_default = true;
-			}
-			rc = q6asm_set_multich_gain(prtd->audio_client,
-				num_channels, gain_list, chmap, use_default);
-			break;
-		case Q6_SUBSYS_AVS2_6:
-			pr_debug("%s: call q6asm_set_lrgain\n", __func__);
-			rc = q6asm_set_lrgain(prtd->audio_client,
-						volume_l, volume_r);
-			if (rc < 0)
-				pr_err("%s: Send LR gain command failed rc=%d\n",
-					__func__, rc);
-			break;
-		case Q6_SUBSYS_INVALID:
-		default:
-			pr_err("%s: UNKNOWN AVS IMAGE\n", __func__);
-			return rc;
-		}
+		pr_debug("%s: call q6asm_set_lrgain\n", __func__);
+		rc = q6asm_set_lrgain(prtd->audio_client, volume_l, volume_r);
+		if (rc < 0)
+			pr_err("%s: Send LR gain command failed rc=%d\n",
+				__func__, rc);
 	}
+
 	if (rc < 0)
 		pr_err("%s: Send vol gain command failed rc=%d\n",
 		       __func__, rc);
@@ -1808,29 +1757,9 @@ static int msm_compr_trigger(struct snd_compr_stream *cstream, int cmd)
 			/*
 			 * Cache this time as last known time
 			 */
-			switch (q6core_get_avs_version()) {
-			case (Q6_SUBSYS_AVS2_7):
-			{
-				q6asm_get_session_time(prtd->audio_client,
+			q6asm_get_session_time(prtd->audio_client,
 					       &prtd->marker_timestamp);
-				break;
-			}
-			case (Q6_SUBSYS_AVS2_6):
-			{
-				q6asm_get_session_time_legacy(
-				prtd->audio_client, &prtd->marker_timestamp);
-				break;
-			}
-			case (Q6_SUBSYS_INVALID):
-			default:
-			{
-				pr_err("%s: UNKNOWN AVS IMAGE VERSION\n",
-							__func__);
-				break;
-			}
-			}
 			spin_lock_irqsave(&prtd->lock, flags);
-
 			/*
 			 * Don't reset these as these vars map to
 			 * total_bytes_transferred and total_bytes_available.
@@ -1995,27 +1924,7 @@ static int msm_compr_pointer(struct snd_compr_stream *cstream,
 			pr_debug("%s session time in gapless transition",
 				 __func__);
 
-		switch (q6core_get_avs_version()) {
-		case (Q6_SUBSYS_AVS2_7):
-		{
-			rc = q6asm_get_session_time(prtd->audio_client,
-				       &timestamp);
-			break;
-		}
-		case (Q6_SUBSYS_AVS2_6):
-		{
-			rc = q6asm_get_session_time_legacy(
-					prtd->audio_client, &timestamp);
-			break;
-		}
-		case (Q6_SUBSYS_INVALID):
-		default:
-		{
-			pr_err("%s: UNKNOWN AVS IMAGE VERSION\n", __func__);
-			rc = -EINVAL;
-			break;
-		}
-		}
+		rc = q6asm_get_session_time(prtd->audio_client, &timestamp);
 		if (rc < 0) {
 			pr_err("%s: Get Session Time return value =%lld\n",
 				__func__, timestamp);
